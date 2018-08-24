e4ab6a54c3fa3d5335e68356c01a85841b638e54
Ali Mahfud <alimahfudpr@gmail.com>
2018-08-24 22:11:18 +0700

    [ALI] trying to implement splash screen

---
+++ app/src/main/java/me/mahfud/tokonews/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package me.mahfud.tokonews.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import me.mahfud.tokonews.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
